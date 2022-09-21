8b6c53fc41924b3948f90cc475a37464d77aa309
ilovehleb <108723953+ilovehleb@users.noreply.github.com>
2022-09-21 20:44:50 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,10 +5,10 @@ void SecondArrayWithIF(string[] arr1, string[] arr2)
     int count = 0;
     for (int i = 0; i < arr1.Length; i++)
     {
-    if(arr1[i].Length <= 3)
+        if(arr1[i].Length <= 3)
         {
-        arr2[count] = arr1[i];
-        count++;
+            arr2[count] = arr1[i];
+            count++;
         }
     }
 }
