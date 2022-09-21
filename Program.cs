91eb87158012d4111cdeb4e460bc529125c4842b
ilovehleb <ilovehleb@gmail.com>
2022-09-21 12:08:19 +0300

    Код.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] arr2 = new string[arr1.Length];
+void SecondArrayWithIF(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
